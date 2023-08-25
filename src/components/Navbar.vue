45eefd7f3ee3994ed3156b3aa755b7693bede13d
ali haider <alibarcha910@gmail.com>
2023-08-25 21:56:40 +0500

    complete navbar design

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,50 +1,22 @@
 <template>
-  <!-- <header class="bg-white shadow-md h-16 flex justify-between items-stretch">
-    <router-link
-      :to="{ name: 'home' }"
-      class="inline-flex items-center h-full px-5 text-2xl font-bold text-red-500"
-      >Recipes</router-link
-    >
-    <div>
-      <router-link
-        :to="{ name: 'byName' }"
-        class="inline-flex items-center px-2 h-full hover:bg-purple-200"
-        >Search By Name</router-link
-      >
-      <router-link
-        :to="{ name: 'byLetter' }"
-        class="inline-flex items-center px-2 h-full hover:bg-purple-200"
-        >Meals By Letter</router-link
-      >
-      <router-link
-        :to="{ name: 'byIngredient' }"
-        class="inline-flex items-center px-2 h-full hover:bg-purple-200"
-        >Meals By Ingredients</router-link
-      >
-       <router-link
-        :to="{ name: 'ingredient' }"
-        class="inline-flex items-center px-2 h-full hover:bg-purple-200"
-        >Ingredients</router-link
-      >
-    </div>
-  </header> -->
 
   <nav class="bg-white border-gray-200 dark:bg-gray-900 shadow-md">
     <div
-      class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+      class="max-w-screen-xl flex lg:flex-nowrap flex-wrap items-center justify-between mx-auto p-4"
     >
       <router-link
         :to="{ name: 'home' }"
         class="flex items-center text-2xl font-bold text-red-500"
       >
-        <img src="../assets/food.png" class="h-8 mr-3" alt="Logo" />
+        <img src="../assets/food.png" class="h-8 mr-1" alt="Logo" />
         <span
           class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
           >Recipes</span
         >
       </router-link>
-      <div class="flex items-center md:order-2">
+      <div class="flex items-center lg:order-3 relative">
         <button
+          @click="openUserProfile"
           type="button"
           class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
           id="user-menu-button"
@@ -54,23 +26,24 @@
         >
           <span class="sr-only">Open user menu</span>
           <img
-            class="w-8 h-8 rounded-full"
+            class="w-10 h-10 rounded-full"
             src="https://randomuser.me/api/portraits/men/78.jpg"
             alt="user photo"
           />
         </button>
         <!-- Dropdown menu -->
         <div
-          class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-          id="user-dropdown"
+          v-if="profile"
+          class="z-50 absolute right-0 top-8 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+          id="user-dropdow"
         >
           <div class="px-4 py-3">
             <span class="block text-sm text-gray-900 dark:text-white"
-              >Bonnie Green</span
+              >alibarcha</span
             >
             <span
               class="block text-sm text-gray-500 truncate dark:text-gray-400"
-              >name@flowbite.com</span
+              >alibarcha910@gmail.com</span
             >
           </div>
           <ul class="py-2" aria-labelledby="user-menu-button">
@@ -105,6 +78,7 @@
           </ul>
         </div>
         <button
+          @click="toggleDropdown"
           data-collapse-toggle="navbar-user"
           type="button"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -113,7 +87,7 @@
         >
           <span class="sr-only">Open main menu</span>
           <svg
-            class="w-5 h-5"
+            class="w-6 h-6"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -130,46 +104,47 @@
         </button>
       </div>
       <div
-        class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+        v-if="dropdownMenu"
+        class="items-center md:relative absolute md:justify-center md:left-auto left-0 md:right-auto right-0 md:top-auto top-20 z-40 md:shadow-none shadow-xl justify-between md:w-full w-11/12 mx-auto md:flex md:order-1 md:my-5 my-auto"
         id="navbar-user"
       >
         <ul
-          class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          class="flex flex-col font-medium p-4 md:p-0  border-gray-100 bg-gray-100 md:flex-row md:space-x-8 md:mt-0 md:border-0  md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
         >
-          <li>
+          <li class="md:mb-0 mb-2">
             <router-link
               :to="{ name: 'home' }"
-              class="block py-2 pl-3 pr-4 text-gray-900 bg-blue-700 rounded md:bg-transparent md:hover:text-red-500 md:p-0 md:dark:text-blue-500"
+              class="block py-2 pl-3 pr-4 md:text-gray-900 text-white bg-red-500 rounded md:bg-transparent md:hover:text-red-500 md:p-0 md:hover:bg-transparent hover:bg-blue-400 md:dark:text-blue-500"
               >Home
             </router-link>
           </li>
-          <li>
+          <li class="md:mb-0 mb-2">
             <router-link
               :to="{ name: 'byName' }"
-              class="block py-2 pl-3 pr-4 text-gray-900 bg-blue-700 rounded md:bg-transparent md:hover:text-red-500 md:p-0 md:dark:text-blue-500"
+              class="block py-2 pl-3 pr-4 md:text-gray-900 text-white bg-red-500 rounded md:bg-transparent md:hover:text-red-500 md:p-0 md:dark:text-blue-500 md:hover:bg-transparent hover:bg-blue-400"
               aria-current="page"
             >
               Meals By Name
             </router-link>
           </li>
-          <li>
+          <li class="md:mb-0 mb-2">
             <router-link
               :to="{ name: 'byLetter' }"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="block py-2 pl-3 pr-4 md:text-gray-900 text-white bg-red-500 md:bg-transparent rounded md:hover:text-red-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 md:hover:bg-transparent hover:bg-blue-400"
               >Meals By Letters</router-link
             >
           </li>
-          <li>
+          <li class="md:mb-0 mb-2">
             <router-link
               :to="{ name: 'byIngredient' }"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="block py-2 pl-3 pr-4 md:text-gray-900 text-white bg-red-500 md:bg-transparent rounded md:hover:text-red-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 md:hover:bg-transparent hover:bg-blue-400"
               >Meals By Ingredients</router-link
             >
           </li>
           <li>
             <router-link
               :to="{ name: 'ingredient' }"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="block py-2 pl-3 pr-4 md:text-gray-900 text-white bg-red-500 md:bg-transparent rounded md:hover:text-red-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 md:hover:bg-transparent hover:bg-blue-400"
               >Ingredients</router-link
             >
           </li>
@@ -180,6 +155,22 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
+
+//user profile
+const profile = ref(false);
+const openUserProfile = () => {
+  profile.value = !profile.value;
+};
+
+// dropdownMenu
+const dropdownMenu = ref(true);
+
+
+
+const toggleDropdown = () => {
+  dropdownMenu.value = !dropdownMenu.value;
+};
 </script>
 
 <style>
